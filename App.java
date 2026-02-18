21447623084a91a132c5a997bb30c673c68e5bb7
Bunny <Bunny45@gmail.com>
2026-02-18 10:45:44 +0000

    modified by darvesh

--- App.java
+++ App.java
@@ -5,5 +5,6 @@ public class APP {
 		System.out.println("I Love You ^.^ ");
 		System.out.println("Sub is:"+(a-b));
 		System.out.println("Eid Mubarak");
+		System.out.println("Multiplication is:"+(a*b));
 	}
 }

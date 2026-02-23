e7f7a4a37adf3c44917f5bd6f89052ab4156db7a
darvesh77 <shaikdarwesh1@gmail.com>
2026-02-23 17:45:04 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,10 +1,5 @@
-public class APP {
-	public static void main (String[] args){
-		int a=10, int b=2;
-		System.out.println("Addition is:"+(a+b));
-		System.out.println("I Love You ^.^ ");
-		System.out.println("Sub is:"+(a-b));
-		System.out.println("Eid Mubarak");
-		System.out.println("Multiplication is:"+(a*b));
-	}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
